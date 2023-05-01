ae6034028630aee8c37b0082bc4cb7773e3e0cf4
hanbin <hanbin.9797@gmail.com>
2023-05-01 17:42:26 +0200

    [pom] init t2

---
+++ pom/hlp-3idx/hlp-3idx.py
@@ -0,0 +1,65 @@
+from gurobipy import *
+import json
+
+# You do not need to modify this function
+def read_instance(instance_path):
+    with open(instance_path) as f:
+        data = json.load(f)
+    return data["p"], data["c"], data["alpha"], data["customers"], data["distances"], data["demands"]
+
+def solve(p, c, alpha, customers, distances, demands):
+    """Solves the hub location problem using the 3-index formulation.
+
+    Args:
+        p (int): number of hubs to open
+        c (float): cost factor for flows between customer and hub in money units per demand and distance unit
+        alpha (float): discount factor for intra-hub transports
+        customers (list[int]): list of customer indices
+        distances (list[list[float]]): distance matrix, distances[i][j] is the distance between customer i and customer j
+        demands (list[list[float]]): demand matrix, demands[i][j] is the demand from customer i to customer j
+
+    Returns:
+        Gurobi.Model: the solved model
+    """    
+    model = Model("Hub Location")
+
+    # You can try and disable cutting planes - what happens?
+    # model.setParam(GRB.Param.Cuts, 0)
+    # NOTE: Please do not turn off cutting planes in your final submission
+
+    # --- Do we need to prepare something? ---
+
+    # --- Variables ---
+
+    # y = tupledict()
+    # for ... 
+        # y[i, k, l] = model.addVar(vtype="c", lb=0, name=f"y_{i}_{k}_{l}",
+        # obj = ...)
+
+    # more variables are necessary
+
+    model.update()
+
+    # --- Constraints ---
+
+
+    # --- Solve model ---
+
+    # If you want to solve just the LP relaxation, uncomment the lines below
+    # model.update()
+    # model = model.relax()
+
+    model.optimize()
+    # model.write("model.lp")
+
+    # If your model is infeasible (but you expect it to not be), comment out the lines below to compute and write out a infeasible subsystem (Might take very long)
+    # model.computeIIS()
+    # model.write("model.ilp")
+
+    return model
+
+
+if __name__ == "__main__":
+    p, c, alpha, customers, distances, demands = read_instance("n10.json")
+
+    solve(p, c, alpha, customers, distances, demands) 
